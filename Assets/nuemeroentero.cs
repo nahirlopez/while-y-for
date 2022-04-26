72fd25c73c77da82ccc51a4947e948c523e4fc4f
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-04-26 09:03:03 -0300

    ej pizarron

---
+++ Assets/nuemeroentero.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nuemeroentero : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num1>=num2)
+        {
+            Debug.Log("Error");
+        }
+        else
+        {
+while (num2 >=num1)
+        {
+                Debug.Log(num2);
+                num2--;
+        }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
